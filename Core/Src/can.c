d0db4b0d8de97bc1dcf14b1a79463e9f6da0dcc7
Yhuuk <3359130875@qq.com>
2026-08-19 14:46:51 +0800

    只是加入了FreeRtos,还没调度任务

--- Core/Src/can.c
+++ Core/Src/can.c
@@ -84,7 +84,7 @@ void HAL_CAN_MspInit(CAN_HandleTypeDef* canHandle)
     HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
 
     /* CAN1 interrupt Init */
-    HAL_NVIC_SetPriority(CAN1_TX_IRQn, 0, 0);
+    HAL_NVIC_SetPriority(CAN1_TX_IRQn, 5, 0);
     HAL_NVIC_EnableIRQ(CAN1_TX_IRQn);
   /* USER CODE BEGIN CAN1_MspInit 1 */
 
